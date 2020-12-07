ccb98479c22ff992a69634b86750a38455c8d079
Shubhanshu-web <70848336+Shubhanshu-web@users.noreply.github.com>
2020-12-07 23:23:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+gsap.fromTo('#bg nav',{y:-100,opacity:0},{duration:1,ease:'expo.InOut',opacity:1,y:0});
+gsap.fromTo('#left',{x:-100,opacity:0},{duration:1,ease:'expo.InOut',opacity:1,x:0});
+gsap.fromTo('#right',{x:100,opacity:0},{duration:1,ease:'expo.InOut',opacity:1,x:0});
+gsap.fromTo('#midmnl',{y:-100,opacity:0},{duration:1,ease:'expo.InOut',opacity:1,y:0});
+gsap.fromTo('#midmnr',{y:100,opacity:0},{duration:1,ease:'expo.InOut',opacity:1,y:0});
